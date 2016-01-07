a75b45d16c542d9bb6d00bdf312a4c485df1dd7f
Nino Rasic <nino.rasic@gmail.com>
2016-01-07 12:55:05 +0100

    Updating edge info

--- Bioinfo/MapEdge.h
+++ Bioinfo/MapEdge.h
@@ -1,6 +1,7 @@
 #ifndef MAPEDGE_H
 #define MAPEDGE_H
 
+#include <iostream>
 
 class MapEdge
 {
@@ -12,10 +13,23 @@ public:
     int readB; //id of second fragment
     int ahang;
     int bhang;
+    int aStart;
+    int aEnd;
+    int bStart;
+    int bEnd;
+    int APlus;
+    int AMinus;
+    int BPlus;
+    int bMinus;
+    int aLenghth;
+    int bLenght;
     int oLenght; //lenght of overlap
     int edgeLenght; //lenght of edge
     int orientationA; //orientation of readA
     int orientationB; //orientation of readB
+    int oB;
+    int first;
+    std::string edgeInfo;
 
 
 };
